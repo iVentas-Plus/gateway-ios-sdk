e3204b56821b1d0fa56de61185fecdb4dda77189
Miguel Acosta <migue.un.av@gmail.com>
2024-11-08 16:31:24 -0600

    plist

---
+++ MPGSDK/MPGSDK.h
@@ -0,0 +1,19 @@
+//
+//  MPGSDK.h
+//  MPGSDK
+//
+//  Created by Miguel Acosta on 08/11/24.
+//  Copyright © 2024 {ORGANIZATION}. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MPGSDK.
+FOUNDATION_EXPORT double MPGSDKVersionNumber;
+
+//! Project version string for MPGSDK.
+FOUNDATION_EXPORT const unsigned char MPGSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MPGSDK/PublicHeader.h>
+
+
