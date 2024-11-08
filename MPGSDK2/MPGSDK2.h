0fd9efec81f295de52ccf292b3b431a102f30d62
Miguel Acosta <migue.un.av@gmail.com>
2024-11-08 16:53:23 -0600

    configs

---
+++ MPGSDK2/MPGSDK2.h
@@ -0,0 +1,19 @@
+//
+//  MPGSDK2.h
+//  MPGSDK2
+//
+//  Created by Miguel Acosta on 08/11/24.
+//  Copyright © 2024 {ORGANIZATION}. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MPGSDK2.
+FOUNDATION_EXPORT double MPGSDK2VersionNumber;
+
+//! Project version string for MPGSDK2.
+FOUNDATION_EXPORT const unsigned char MPGSDK2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MPGSDK2/PublicHeader.h>
+
+
